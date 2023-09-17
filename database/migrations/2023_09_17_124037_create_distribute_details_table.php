99d8cdd6824c728c3672b72edf8399c236afe655
jobayermahamud <jobayer@mail.com>
2023-09-17 12:49:33 +0600

    distribute model & migration

---
+++ database/migrations/2023_09_17_124037_create_distribute_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistributeDetailsTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('distribute_details', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('distribute_id');
+            $table->bigInteger('product_id');
+            $table->integer('demand_quantity')->nullable();
+            $table->integer('approve_quantity')->nullable();
+            $table->longText('remarks')->nullable();
+            $table->integer('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('distribute_details');
+    }
+}
