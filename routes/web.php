fe7b23bb025777a82a1c1505e5630c7f602b4e21
mohammad alamin <mohammadalamin.dev@gmail.com>
2023-09-17 15:34:27 +0600

    web route modify

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('storage-link',function(){
+    \Artisan::call('storage:link');
+});
+
 Route::get('/', function () {
     // return view('frontend.home');
     return redirect()->route('admin.login');
