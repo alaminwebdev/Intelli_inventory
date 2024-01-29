71a93f68ef03cbe0739fdf219510c2e0d20003aa
Mohammad Alamin <mohammadalamin.dev@gmail.com>
2024-01-29 14:44:40 +0600

    product list modify

--- app/Models/ProductInformation.php
+++ app/Models/ProductInformation.php
@@ -11,4 +11,7 @@ class ProductInformation extends Model
     public function unit() {
         return $this->hasOne(Unit::class, 'id', 'unit_id' );
     }
+    public function product_type() {
+        return $this->hasOne(ProductType::class, 'id', 'product_type_id' );
+    }
 }
