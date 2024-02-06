31a500502921e1cb0b8415d7f6eea7bf260e4aa1
Mohammad Alamin <mohammadalamin.dev@gmail.com>
2024-02-06 18:25:24 +0600

    stock in edit and search option

--- app/Models/StockInDetail.php
+++ app/Models/StockInDetail.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class StockInDetail extends Model
 {
     use HasFactory;
+
+    public function product() {
+        return $this->hasOne(ProductInformation::class, 'id', 'product_information_id' );
+    }
 }
